f04f783c1d882b37f2b114e413c985c9b4477041
vilarj <lamaravillasan@outlook.com>
2024-06-02 11:07:22 -0400

    Cleaning up Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,5 @@
-import Problems.cigarParty;
-
 public class Driver {
     public static void main(String[] args) {
-        cigarParty c = new cigarParty();
 
-        System.out.println(c.cigars(30, false)); // false
-        System.out.println(c.cigars(50, false)); // true
-        System.out.println(c.cigars(70, true)); // true
-        System.out.println(c.cigars(30, true)); // false
-        System.out.println(c.cigars(39, true)); // false
     }
 }
